3c6561702c3d547c4cbbdf0d9c715420224c0bf0
AlexAntoine <antoinealex1234@gmial.com>
2021-04-11 22:57:04 -0400

    configuration for passport is complete

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose =require('mongoose');
-// const encrypt = require('mongoose-encryption');
 const ejs = require('ejs');
+//require these three packages
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
-// const bcrypt = require('bcrypt');
-// const saltRounds = 10;
-// const md5 = require('md5'); //hashing 
 
 const app = express();
 
@@ -18,20 +15,44 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, ()=>{
-    console.log('listening on port 3000');
-});
+/*
+The order of the code is important 
+*/
+
+//sessions set up
+app.use(session({
+    secret: 'Mmjwtbahyk88',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//initialize passport
+app.use(passport.initialize());
+//use passport to manage sessions
+app.use(passport.session());
+
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String, 
 });
 
-//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+//set up userSchema to use passport local mongoose
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+//create a local login stategy
+passport.use(User.createStrategy());
+
+//set up passport serializer and deserilaieze to set up user 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 const alex = new User({
     email: 'antoinealex1234@gmail.com',
@@ -61,4 +82,8 @@ app.post('/register', (req, res)=>{
 app.post('/login', (req, res)=>{
 
    
+});
+
+app.listen(3000, ()=>{
+    console.log('listening on port 3000');
 });
